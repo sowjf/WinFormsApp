82865d23377fc10acfa122a0f0ff26a88cedcafc
Тим <@WIN-TUP5OPEF9K4>
2025-10-19 22:20:36 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,24 @@
+using System.Drawing;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Graphics g;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormsApp/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace WinFormsApp1 {
+        public abstract class Shape {
+            protected int x, y;
+            protected static int R = 5;
+
+            public Shape(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+
+            public abstract void draw();
+        }
+
+        public class Circle : Shape {
+            public Circle(int x, int y) : base(x, y) { }
+
+            public override void draw() { }
+        }
+
+        public class Triangle : Shape {
+            public Triangle(int x, int y) : base(x, y) { }
+
+            public override void draw() { }
+        }
+
+        public class Square : Shape {
+            public Square(int x, int y) : base(x, y) { }
+
+            public override void draw() { }
+        }
+    }
+}
