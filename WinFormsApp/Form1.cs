2ed3404084d157c8855f606009323a55ea6911d3
Tim <speshilov.timofey@lit1533.ru>
2025-10-20 14:13:38 +0300

    homework + lesson

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -1,24 +1,35 @@
 using System.Drawing;
+using WinFormsApp;
 
 namespace WinFormsApp
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
+        private Shape S;
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        public Form1() {
             InitializeComponent();
+
+            S = new Square(200, 200);
+
+            Console.WriteLine(S.IsInside(100, 100));
+            this.Paint += Form1_Paint;
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        Graphics g;
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            // Вызываем Draw от S, передав e.Graphics
+            S.Draw(e.Graphics);
+        }
 
+
+        private void button1_Click(object sender, EventArgs e) {
+            Graphics g = this.CreateGraphics();
+            g.DrawEllipse(new Pen(Color.Red), 100, 100, 200, 200);
         }
     }
 }
--- WinFormsApp/Shape.cs
+++ WinFormsApp/Shape.cs
@@ -1,44 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WinFormsApp
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    public abstract class Shape {
+        protected int x, y;
+        protected static int R;
 
-    namespace WinFormsApp1 {
-        public abstract class Shape {
-            protected int x, y;
-            protected static int R = 5;
-
-            public Shape(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-
-            public abstract void draw();
+        public Shape(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
-        public class Circle : Shape {
-            public Circle(int x, int y) : base(x, y) { }
+        static Shape() { R = 50; }
+        public abstract void Draw(Graphics g);
+        public abstract bool IsInside(int pointX, int pointY);
+    }
 
-            public override void draw() { }
+    public class Circle : Shape {
+        public Circle(int x, int y) : base(x, y) { }
+
+        public override void Draw(Graphics g) { g.DrawEllipse(Pens.Black, this.x - R, this.y - R, 2 * R, 2 * R); }
+
+        public override bool IsInside(int pointX, int pointY) { return (pointX - x) * (pointX - x) + (pointY - y) * (pointY - y) <= R * R; }
+    }
+
+    public class Triangle : Shape {
+        public Triangle(int x, int y) : base(x, y) { }
+
+        public override void Draw(Graphics g) {
+            Point[] points = {
+                new Point(x, y - R), // up
+                new Point(x - R, y + R), // left
+                new Point(x + R, y + R) // right
+            };
+
+           g.DrawPolygon(Pens.Black, points);
         }
 
-        public class Triangle : Shape {
-            public Triangle(int x, int y) : base(x, y) { }
-
-            public override void draw() { }
-        }
-
-        public class Square : Shape {
-            public Square(int x, int y) : base(x, y) { }
-
-            public override void draw() { }
+        public override bool IsInside(int pointX, int pointY) { 
+            int a = (int)(Math.Sqrt(R * R / 2));
+            if ((pointX < this.x + a) & (pointX > this.x - a) & (pointX < y + a) & (pointY > y + a)) { return true; }
+            return false;
         }
     }
+
+    public class Square : Shape {
+        public Square(int x, int y) : base(x, y) { }
+
+        public override void Draw(Graphics g) { g.DrawRectangle(Pens.Black, x - R, y - R, 2 * R, 2 * R); }
+
+        public override bool IsInside(int pointX, int pointY) { return pointX >= x - R && pointX <= x + R && pointY >= y - R && pointY <= y + R; }
+    }
+
 }
